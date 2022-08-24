b3218d731999a8974865a91459843cc2a42af1ab
Jagriti Sharma <jaagi.sharma@gmail.com>
2022-08-24 14:15:53 +0300

    temperature task start

--- project/task_gasoline/script.js
+++ project/task_gasoline/script.js
@@ -11,14 +11,12 @@ let answer1= document.querySelector('#answer');
 let cal= Math.floor(money/ price);
 let text;
 if (cal>=10)
-{
-text =  'good, you can escape now'
-}
-else {
+ {
+text =  "good, you can escape now"
+ }
+ else {
     text = "ups, you have to stay here."
 }
 answer1.textContent = text;
-
 }
 
-/*  amount>=10
