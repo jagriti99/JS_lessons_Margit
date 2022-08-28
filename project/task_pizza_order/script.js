477df4f1373e0ef6ef015bbe58925236fe893661
Sharma Jagriti <s2200131@BC-C02J61DSQ6LR-MacBook-Air.local>
2022-08-28 19:47:29 +0300

    pizza tak

---
+++ project/task_pizza_order/script.js
@@ -0,0 +1,24 @@
+function order() {
+  const name = document.querySelector("custName").value;
+  //const size=+(document.querySelector("psize").value);
+  //   let topping = document.querySelector().value;
+  //   let count;
+  //   let price = document.forms[0];
+  //   let text = "";
+
+  //   for (let i = 0; i < price.length; i++) {
+  //     if (price[i].checked) {
+  //       text = text + price[i].value + " ";
+  //     }
+  //   }
+
+  console.log(name);
+}
+
+// for (let i =1; i<= 8; i++){
+// if (count>4){
+
+// }else {
+//     return "first four topping are free"
+
+// }
