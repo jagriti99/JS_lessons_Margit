d65f01ed0c687237dbc75a5a4cbc57232e6e8150
Sharma Jagriti <s2200131@BC-C02J61DSQ6LR-MacBook-Air.local>
2022-08-31 14:37:50 +0300

    insurence task

--- project/task_pizza_order/script.js
+++ project/task_pizza_order/script.js
@@ -1,1 +1,28 @@
-function order() {}
+const form = document.querySelector("form");
+const customer = document.querySelector("#name");
+const size = document.querySelectorAll('[name="size"]');
+const topping = document.querySelectorAll('input [type ="checkbox"]');
+
+console.log(customer);
+
+const takeOrder = (event) => {
+  event.preventDefault();
+
+  let customerName = customer.value;
+  let sizeResult = "";
+  size.forEach((item) => {
+    sizeResult = item.value;
+  });
+ topping.forEach((item) => {
+ if (item.checked){
+  toppingResult.push(item.value);
+ }
+
+ }
+ }
+
+};
+
+
+
+form.addEventListener("submit", takeOrder);
